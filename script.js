65eae1f28b68f08c6fd1b45cc3047aac9a0c6701
Collin Whalen <collinw@ihspharmacy.com>
2022-05-08 19:16:32 -0500

    working on collison detection, and fixing a projectile firing bug'

--- script.js
+++ script.js
@@ -37,90 +37,152 @@ class Projectile {
   }
 
   update() {
-    this.draw()
+    this.draw();
     this.x = this.x + this.velocity.x;
     this.y = this.y + this.velocity.y;
-
   }
 }
 
 class Enemy {
-    constructor(x, y, radius, color, velocity) {
-      this.x = x;
-      this.y = y;
-      this.radius = radius;
-      this.color = color;
-      this.velocity = velocity;
-    }
-  
-    draw() {
-      c.beginPath();
-      c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-      c.fillStyle = this.color;
-      c.fill();
-    }
-  
-    update() {
-      this.draw()
-      this.x = this.x + this.velocity.x;
-      this.y = this.y + this.velocity.y;
-  
-    }
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
   }
-  
 
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
 
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "blue");
 
-
-
 const projectile = new Projectile(
   canvas.width / 2,
   canvas.height / 2,
   5,
   "red",
-  { 
+  {
     x: 1,
-    y: 1
+    y: 1,
   }
 );
 
-const projectiles = [
+const projectiles = [];
+const enemies = [];
 
-]
+function spawnEnemies() {
+  setInterval(() => {
+    const radius =  Math.random() * ( 30 - 4 ) + 4
+    let x;
+    let y;
 
-function animate() {
-    requestAnimationFrame(animate);
-    c.clearRect(0, 0, canvas.width, canvas.height)
-    player.draw();
-  projectiles.forEach((projectile) => {
-      projectile.update()
-
-  })
-  }
-
-addEventListener("click", (e) => {
-
-    const angle = Math.atan2(e.clientY - canvas.height / 2, e.clientX - canvas.width / 2)
-
-
-    const velocity = {
-        x: Math.cos(angle),
-        y: Math.sin(angle)
+    if (Math.random() < 5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius;
+      y = Math.random() * canvas.height;
+    } else {
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius;
+      x = Math.random() * canvas.width;
     }
 
-    console.log(angle)
-    projectiles.push(new Projectile(
-        canvas.width / 2, canvas.height / 2, 5, 'red' , velocity
-    ))
+   
+    const color = "green";
+    // const velocity = {
+    //   x: 1,
+    //   y: 1,
+    // };
 
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle),
+    };
+
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+    // console.log(ememies);
+  }, 1000);
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+
+  enemies.forEach((enemy) => {
+    enemy.update();
+
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+    // console.log(dist + "first this is dist");
+    if (dist - enemy.radius - player.radius < 1) {
+      console.log( "end game");
+    }
+      
+
+    projectiles.forEach((enemy, index) => {
+
+       
+
+        projectiles.forEach((projectile, projectileIndex) => {
+           const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+           console.log(dist + " this is dist");
+           
+           if(dist - enemy.radius - projectile.radius < 1) {
+
+            setTimeout(() => {
+                enemies.splice(index, 1)
+                projectiles.splice(projectileIndex, 1)
+
+            }, 0)
+           
+           }
+           
+        })
+    })
+
+  });
+}
+
+addEventListener("click", (e) => {
+  const angle = Math.atan2(
+    e.clientY - canvas.height / 2,
+    e.clientX - canvas.width / 2
+  );
+
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle),
+  };
+
+  console.log(angle);
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 5, "red", velocity)
+  );
 });
 
 animate();
+spawnEnemies()
 
 console.log(player);
 console.log(canvas);
 console.log(c);
+
+// could add a shop for upgrades, like a random shooter, time slower, shotgun mode
