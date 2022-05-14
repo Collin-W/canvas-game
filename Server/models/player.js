ed60e643a3a54e0a2fa81b5ec460b98c8c4527b7
Collin Whalen <collinw@ihspharmacy.com>
2022-05-14 10:07:19 -0500

    commiting config file to push to main

--- Server/models/player.js
+++ Server/models/player.js
@@ -1,18 +1,41 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const playerSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true
-    },
-    currentScore: {
-        type: Number,
-        required: false
-    },
-    highScore: {
-        type: Number,
-        required: false
-    }
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+  //default scores at 0
+  currentScore: {
+    type: Number,
+    required: true,
+  },
+  highScore: {
+    type: Number,
+    required: true,
+  },
+});
 
-module.exports = mongoose.model('Player', playerSchema)
+//need to set up bycrpt still
+// //middleware for password creatiion
+// playerSchema.pre('save', async function(next) {
+//     if(this.isNew || this.isModified('password')) {
+//         const saltRounds = 5;
+//         this.password = await bcrypt.hash(this.password, saltRounds);
+//     }
+//     next();
+// })
+//compare the incoming password with the hashed password
+// playerSchema.methods.isCorrectPassword = async function(password) {
+//     return await bcrypt.compare(password, this.password)
+// }
+
+const Player = mongoose.model("Player", playerSchema);
+
+module.exports = Player;
